74aee2e0d3c8f3c141bb668fcb419185e016189f
Filipe Alves <lipeqa@gmail.com>
2023-11-09 21:27:47 -0300

    add style to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import { Spectral, Creepster } from 'next/font/google'
-const spectral = Spectral({subsets: ['latin'], weight:'400'})
-const creepster = Creepster({subsets: ['latin'],weight:'400'})
+import { spectral700, spectral400} from '../styles/fonts'
+import Image from 'next/image'
+
+import logoHome from '../assets/Logo-home.png'
 export default function Home() {
   return (
-   <h1 className={`${spectral.className}`}>Challenge FRONT-END IPDelve
-   </h1>
+   <main className={` my-0 mx-auto  h-96 flex flex-col justify-around items-start ${spectral700.className}`}>
+    <div className="w-80 flex items-center justify-center">
+    <Image src={logoHome} width={160} className="object-cover" />
+    </div>
+
+    <p className="text-5xl w-96 h-28">Saiba tudo em um só <span className="text-green-rm-300">lugar</span>.</p>
+   <p className={`${spectral400.className} `}>Personagens, localizações, episódios e muito mais.</p>
+   </main>
   )
 }
