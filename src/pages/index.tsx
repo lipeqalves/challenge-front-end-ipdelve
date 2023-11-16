f14c3767f2239bcf5b4841ea312aae8f9c01c11e
Filipe Alves <lipeqa@gmail.com>
2023-11-16 15:17:50 -0300

    refactoring the home page style, making it responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import logoHome from '../assets/Logo-home.png'
 export default function Home() {
   return (
     <section
-      className={` my-0 mx-auto max-w-6xl h-96 flex flex-col justify-around items-center `}
+      className={` my-0 mx-auto max-w-5xl h-96 flex flex-col justify-around items-center `}
     >
       <div className="w-80 flex items-center justify-center">
         <Image
@@ -16,10 +16,10 @@ export default function Home() {
         />
       </div>
 
-      <p className={`text-5xl w-96 h-28 ${spectral700.className}`}>
+      <p className={`text-5xl w-96 text-center md:text-start h-28 ${spectral700.className}`}>
         Saiba tudo em um só <span className="text-green-rm-300">lugar</span>.
       </p>
-      <p className={`${spectral400.className} `}>
+      <p className={`${spectral400.className} text-center md:text-start w-96`}>
         Personagens, localizações, episódios e muito mais.
       </p>
     </section>
